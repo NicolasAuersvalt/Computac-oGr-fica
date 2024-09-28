7c6429b78cbad5e63c8736fd644aff7745213b1d
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2024-09-28 02:20:37 -0300

    Update header.py

--- header.py
+++ header.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
-from Multiplicacao import multiplicar_matrizes
-from Adicao import somar_matrizes
-from Diagonal import diagonal
 import plotly.graph_objs as go
 import math
+from CompGraf import plot_matriz_3d
+from Funcoes import *
